3eba3fa70c8d3788c3db81de999c83dbb6f904ac
dd <sinfere@gmail.com>
2022-06-29 20:23:19 +0800

    fix: println needs a message exception

--- serialport/src/main/java/tp/xmaihh/serialport/SerialHelper.java
+++ serialport/src/main/java/tp/xmaihh/serialport/SerialHelper.java
@@ -111,7 +111,8 @@ public abstract class SerialHelper {
 //                    }
 
                 } catch (Throwable e) {
-                    Log.e("error", e.getMessage());
+                    e.printStackTrace();
+//                    Log.e("error", e.getMessage());
                     return;
                 }
             }
